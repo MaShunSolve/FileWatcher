7914a46d33ccca08761b76a30c1f840a3df82e8c
eddy <eddyhsieh@mail.kway.com.tw>
2022-11-20 14:25:49 +0800

    加入專案檔案。

---
+++ FileWatcher/FileWatcherService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace FileWatcher
+{
+    public class FileWatcherService
+    {
+        public static Dictionary<string, FileSystemWatcher> Watchers = new Dictionary<string, FileSystemWatcher>();
+        #region 啟動監看
+        public void FileWatch(colleague colleague)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            Watchers.Add(colleague.Name, watcher);
+            watcher.Path = WatchingFolder(colleague.Name);//監視目錄
+            watcher.Filter = "*.*";//檔案類型
+            watcher.EnableRaisingEvents = true;
+            watcher.IncludeSubdirectories = false;
+            watcher.Changed += new FileSystemEventHandler((s, e) => OnChanged(s, e, colleague));
+        }
+        public static void OnChanged(object source, FileSystemEventArgs watchEvent, colleague colleague)
+        {
+            var watcher = source as FileSystemWatcher;
+            Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} {colleague.Name} {colleague.Action}");
+        }
+        #endregion
+        /// <summary>
+        /// 路徑
+        /// </summary>
+        /// <param name="job"></param>
+        /// <returns></returns>
+        public string WatchingFolder(string name)
+        {
+            string path = null;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                switch (name.ToUpper())
+                {
+                    case "EDDY":
+                        path = @"C:\Users\user\Desktop\Test\temp3";
+                        break;
+                    case "TOM":
+                        path = @"C:\Users\user\Desktop\Test\temp2";
+                        break;
+                }
+            }
+            else
+            {
+                switch (name.ToUpper())
+                {
+                    case "EDDY":
+                        path = "/home/kway/TestSample/test3";
+                        break;
+                    case "TOM":
+                        path = "/home/kway/TestSample/test2";
+                        break;
+                }
+            }
+            return path;
+        }
+        public void CancelWatcher(string name)
+        {
+            var watcher = Watchers[name];
+            watcher.Dispose();
+        }
+    }
+}
---
+++ FileWatcher/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using Quartz;
+using Quartz.Impl;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace FileWatcher
+{
+    public class Program : IJob
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("這只是一個測試~~");
+            //建立資料
+            colleague eddy = new colleague(){Name = "Eddy", Action = "在辦公室看到一隻馬兒在奔跑" };
+            colleague jack = new colleague() { Name = "Tom", Action = "給他一根香蕉" };
+            /*重新new測試Dictionary正常*/
+            FileWatcherService fs = new FileWatcherService();
+            fs.FileWatch(eddy);
+            FileWatcherService fs2 = new FileWatcherService();
+            fs2.FileWatch(jack);
+            //Quartz處理
+            Quartz.Logging.LogProvider.IsDisabled = true;
+            StdSchedulerFactory factory = new StdSchedulerFactory();
+            IScheduler scheduler = await factory.GetScheduler();
+            await scheduler.Start();//Scheduler Start
+            await scheduler.ScheduleJob(Job1(), Trigger1());
+            await scheduler.ScheduleJob(Job2(), Trigger2());
+            Console.Read(); 
+        }
+        #region Job
+        public static IJobDetail Job1()
+        {
+            IJobDetail? job = null;
+            job = JobBuilder.Create<Program>()
+                                    .WithIdentity("1", "1")
+                                    .Build();
+            job.JobDataMap["msg"] = "馬兒跑走ㄌ";
+            job.JobDataMap["Name"] = "Eddy";
+            return job;
+        }
+        public static IJobDetail Job2()
+        {
+            IJobDetail? job = null;
+            job = JobBuilder.Create<Program>()
+                                    .WithIdentity("2", "2")
+                                    .Build();
+            job.JobDataMap["msg"] = "沒有香蕉了";
+            job.JobDataMap["Name"] = "Tom";
+            return job;
+        }
+        #endregion
+        #region Trigger
+        public static ITrigger Trigger1()
+        { 
+            
+            ISimpleTrigger trigger = TriggerBuilder.Create()
+                            .WithIdentity("1", "1")
+                            .StartNow()
+                            .StartAt(DateTime.Now.AddSeconds(30))
+                            .Build()
+                             as ISimpleTrigger;
+            return trigger;
+        }
+        public static ITrigger Trigger2()
+        {
+            ISimpleTrigger trigger = TriggerBuilder.Create()
+                            .WithIdentity("2", "2")
+                            .StartNow()
+                            .StartAt(DateTime.Now.AddSeconds(20))
+                            .Build()
+                             as ISimpleTrigger;
+            return trigger;
+        }
+        #endregion
+        public async Task Execute(IJobExecutionContext context)
+        {
+            JobDataMap dataMap = context.JobDetail.JobDataMap;
+            string msg = dataMap.GetString("msg");
+            string name = dataMap.GetString("Name");    
+            Console.WriteLine($"{DateTime.Now.ToString("HH:ss")+" "+msg}");
+            FileWatcherService fs = new FileWatcherService();
+            fs.CancelWatcher(name);
+        }
+    }
+    public class colleague
+    { 
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Action
+        /// </summary>
+        public string Action { get; set; }
+    }
+}
